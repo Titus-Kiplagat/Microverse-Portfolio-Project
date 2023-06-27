b1af47ab53ba830122377defde3e7ce5e553f42f
Titus Lagat <lagattyto@gmail.com>
2023-06-27 21:48:11 +0300

    Added mobile menu functionality and smooth scrolling

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-/* eslint-disable no-unused-vars */
+const menuOpenBtn = document.querySelector('.menu-open-btn');
+const menuCloseBtn = document.querySelector('.menu-close-btn');
+const navbarBtns = document.querySelectorAll('.navbar-btn');
+
 const toggleMenu = () => {
   const mobileMenu = document.getElementById('mobile-navbar');
   mobileMenu.style.display = mobileMenu.style.display === 'block' ? 'none' : 'block';
 };
+
+menuOpenBtn.addEventListener('click', toggleMenu);
+menuCloseBtn.addEventListener('click', toggleMenu);
+
+navbarBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    toggleMenu();
+    const targetSectionId = e.target.getAttribute('href');
+    const targetSection = document.querySelector(targetSectionId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
