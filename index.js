7fa16ce9ae10739353ff8997eb2defbf5dea2d7a
Titus Lagat <lagattyto@gmail.com>
2023-06-28 09:13:24 +0300

    Add dynamic display for project cards using javascript

--- index.js
+++ index.js
@@ -20,3 +20,114 @@ navbarBtns.forEach((btn) => {
     }
   });
 });
+
+const projectList = [
+  {
+    id: 1,
+    title: 'Multi-Post Stories Gain+Glory',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tags: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    imageUrl: './assets/img/Img Plaholder.svg',
+    live: 'https://titus-kiplagat.github.io/Microverse-Portfolio-Project/',
+    source: 'https://github.com/Titus-Kiplagat/Microverse-Portfolio-Project',
+  },
+  {
+    id: 2,
+    title: 'Multi-Post Stories Gain+Glory',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tags: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    imageUrl: './assets/img/Img Plaholder.svg',
+    live: 'https://titus-kiplagat.github.io/Microverse-Portfolio-Project/',
+    source: 'https://github.com/Titus-Kiplagat/Microverse-Portfolio-Project',
+  },
+  {
+    id: 3,
+    title: 'Multi-Post Stories Gain+Glory',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tags: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    imageUrl: './assets/img/Img Plaholder.svg',
+    live: 'https://titus-kiplagat.github.io/Microverse-Portfolio-Project/',
+    source: 'https://github.com/Titus-Kiplagat/Microverse-Portfolio-Project',
+  },
+  {
+    id: 4,
+    title: 'Multi-Post Stories Gain+Glory',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tags: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    imageUrl: './assets/img/Img Plaholder.svg',
+    live: 'https://titus-kiplagat.github.io/Microverse-Portfolio-Project/',
+    source: 'https://github.com/Titus-Kiplagat/Microverse-Portfolio-Project',
+  },
+  {
+    id: 5,
+    title: 'Multi-Post Stories Gain+Glory',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tags: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    imageUrl: './assets/img/Img Plaholder.svg',
+    live: 'https://titus-kiplagat.github.io/Microverse-Portfolio-Project/',
+    source: 'https://github.com/Titus-Kiplagat/Microverse-Portfolio-Project',
+  },
+  {
+    id: 6,
+    title: 'Multi-Post Stories Gain+Glory',
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tags: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    imageUrl: './assets/img/Img Plaholder.svg',
+    live: 'https://titus-kiplagat.github.io/Microverse-Portfolio-Project/',
+    source: 'https://github.com/Titus-Kiplagat/Microverse-Portfolio-Project',
+  },
+];
+
+const createCard = (project) => {
+  const card = document.createElement('article');
+  card.className = 'card';
+
+  const cardImage = document.createElement('div');
+  cardImage.className = 'card-image';
+  const image = document.createElement('img');
+  image.src = project.imageUrl;
+  image.alt = 'Project Screenshot';
+  cardImage.appendChild(image);
+
+  const cardContent = document.createElement('div');
+  cardContent.className = 'card-content';
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.className = 'card-title';
+  cardTitle.textContent = project.title;
+
+  const cardItems = document.createElement('ul');
+  cardItems.className = 'card-items';
+  project.tags.forEach((tag) => {
+    const fragment = new DocumentFragment();
+    const cardItem = document.createElement('li');
+    cardItem.className = 'card-item';
+    cardItem.textContent = tag;
+    fragment.appendChild(cardItem);
+    cardItems.appendChild(fragment);
+  });
+
+  const cardButton = document.createElement('button');
+  cardButton.className = 'card-button btn-hover';
+  const cardButtonLink = document.createElement('a');
+  cardButtonLink.href = '#';
+  cardButtonLink.textContent = 'See Project';
+  cardButton.appendChild(cardButtonLink);
+
+  cardContent.appendChild(cardTitle);
+  cardContent.appendChild(cardItems);
+  cardContent.appendChild(cardButton);
+
+  card.appendChild(cardImage);
+  card.appendChild(cardContent);
+
+  return card;
+};
+
+const cardsContainer = document.querySelector('#cards-container');
+projectList.forEach((project) => {
+  const fragment = new DocumentFragment();
+  const card = createCard(project);
+  fragment.appendChild(card);
+  cardsContainer.appendChild(fragment);
+});
